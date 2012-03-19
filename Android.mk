ef6333fac9e4b6a4099a448f856768b920288939
Josh Avroch <josh.avroch@gmail.com>
2012-03-18 21:14:23 -0400

    this is more correct i believe

--- Android.mk
+++ Android.mk
@@ -5,7 +5,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
-LOCAL_CERTIFICATE := superuser
+# We're using a modified Superuser source, I don't want it updated via the Mar.. err Play Store anymore
+# LOCAL_CERTIFICATE := superuser
 
 include $(BUILD_PACKAGE)
 
